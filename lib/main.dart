640ea436a7160b91f00ef83001e15c26f765342c
kamsoft <wizytadomowakamsoft@gmail.com>
2019-11-17 21:59:51 +0100

    First part

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+// import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+//part 1
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
+
+// //Part 2
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final wordPair = WordPair.random();            
+//     return MaterialApp(
+//       title: 'Welcome to Flutter',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Welcome to Flutter'),
+//         ),
+//         body: Center(
+//           child: Text(wordPair.asPascalCase),            
+//         ),
+//       ),
+//     );
+//   }
+// }
+
